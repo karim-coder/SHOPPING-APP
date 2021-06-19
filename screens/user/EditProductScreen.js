88ca566e10fe1b62ef44e13d71ae0381bd4d2826
karim-coder <ab.amiri484@gmail.com>
2021-06-19 14:19:32 +0530

    Added some changes

---
+++ screens/user/EditProductScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const EditProductScreen = () => {
+  return (
+    <View>
+      <Text>The Edit Product Screen!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default EditProductScreen;
